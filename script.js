ed74f92718cae7324d3a599d9e6fd625d91b6a15
“Gbolahan <gbolahansalvador37@gmail.com>
2025-09-17 15:59:02 +0100

    added image to hero

---
+++ script.js
@@ -0,0 +1,7 @@
+let ham = document.querySelector(".icon")
+let link = document.querySelector(".navbar")
+
+function changes(){
+    link.style.display = "block"
+}
+ ham.addEventListener('click', changes)
