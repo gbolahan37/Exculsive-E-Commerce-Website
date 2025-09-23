96c4860659e9241f1a807b48acd3f0ce34a3968e
“Gbolahan <gbolahansalvador37@gmail.com>
2025-09-23 10:59:50 +0100

    Initial commit of frontend project

---
+++ asset/script.js
@@ -0,0 +1,49 @@
+let button = document.querySelector(".scroll-buutton")
+let productContainer = document.querySelector(".flash-sales__products")
+let categroyContainer = document.querySelector("cate-card")
+let categroyCard = document.querySelector("cate-box")
+let productCard = document.querySelector(".product-card")
+let leftButton = document.querySelector(".left-button")
+let rightButton = document.querySelector(".right-button")
+
+function scrollRight (){
+    let containerWidth = productContainer.clientWidth
+    let cardWidth = productCard.clientWidth
+    
+
+    
+    let scrollMovement = Math.floor(containerWidth/cardWidth) * cardWidth
+
+    
+    productContainer.scrollLeft = productContainer.scrollLeft + scrollMovement
+}
+
+function leftSide () {
+    let containerWidth = productContainer.clientWidth
+    let cardWidth = productCard.clientWidth
+
+    let scrollMovement = Math.floor(containerWidth / cardWidth) * cardWidth
+     
+    productContainer.scrollLeft = productContainer.scrollLeft - scrollMovement
+}
+
+function cateScrollRight (){
+    let cateWidth = categroyContainer.clientWidth
+    let cateCard = categroyCard.clientWidth
+
+    let scrollCategroy = Math.floor(cateWidth / cateCard) * cateCard
+    categroyContainer.scrollLeft = productContainer.scrollLeft + scrollCategroy
+}
+
+function cateScrollLeft (){
+    let cateWidth = categroyContainer.clientWidth
+    let cateCard = categroyCard.clientWidth
+
+    let scrollCategroy = Math.floor(cateWidth / cateCard) * cateCard
+    categroyContainer.scrollLeft = productContainer.scrollLeft - scrollCategroy
+}
+
+rightButton.addEventListener("click", scrollRight)
+rightButton.addEventListener("click", cateScrollRight)
+
+leftButton.addEventListener("click", leftSide)
