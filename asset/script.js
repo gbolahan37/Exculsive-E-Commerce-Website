062228b29b54ac1a13b003ed5937e35639c8a7a6
“Gbolahan <gbolahansalvador37@gmail.com>
2025-09-23 15:32:53 +0100

    i finshed the new arrival section

--- asset/script.js
+++ asset/script.js
@@ -5,45 +5,45 @@ let categroyCard = document.querySelector("cate-box")
 let productCard = document.querySelector(".product-card")
 let leftButton = document.querySelector(".left-button")
 let rightButton = document.querySelector(".right-button")
+let cateRightButton = document.querySelector(".cate-right-button")
+let cateLeftButton = document.querySelector(".cate-left-button")
 
 function scrollRight (){
     let containerWidth = productContainer.clientWidth
     let cardWidth = productCard.clientWidth
-    
-
-    
     let scrollMovement = Math.floor(containerWidth/cardWidth) * cardWidth
 
-    
     productContainer.scrollLeft = productContainer.scrollLeft + scrollMovement
 }
 
 function leftSide () {
     let containerWidth = productContainer.clientWidth
     let cardWidth = productCard.clientWidth
-
     let scrollMovement = Math.floor(containerWidth / cardWidth) * cardWidth
      
     productContainer.scrollLeft = productContainer.scrollLeft - scrollMovement
 }
 
+// categroyCard Stroll 
+
 function cateScrollRight (){
     let cateWidth = categroyContainer.clientWidth
     let cateCard = categroyCard.clientWidth
-
     let scrollCategroy = Math.floor(cateWidth / cateCard) * cateCard
+
     categroyContainer.scrollLeft = productContainer.scrollLeft + scrollCategroy
 }
 
 function cateScrollLeft (){
     let cateWidth = categroyContainer.clientWidth
     let cateCard = categroyCard.clientWidth
-
     let scrollCategroy = Math.floor(cateWidth / cateCard) * cateCard
+
     categroyContainer.scrollLeft = productContainer.scrollLeft - scrollCategroy
 }
 
 rightButton.addEventListener("click", scrollRight)
-rightButton.addEventListener("click", cateScrollRight)
-
 leftButton.addEventListener("click", leftSide)
+
+cateRightButton.addEventListener("click", cateScrollRight)
+cateLeftButton.addEventListener("click", cateScrollLeft)
